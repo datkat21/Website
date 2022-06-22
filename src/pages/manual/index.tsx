71dcfdb7f44543d8c7552e377c96336d65a360fa
Kat21 <49850194+datkat21@users.noreply.github.com>
2022-06-21 20:31:01 -0500

    Hopefully fix #43
    I missed the .content class that already existed, so I renamed it to .page-content. It may also be sufficient to use an article or section tag instead of a div, but I'm not sure for now.

--- src/pages/manual/index.tsx
+++ src/pages/manual/index.tsx
@@ -1,6 +1,6 @@
 // prettier-ignore
 
 const Download = () => {
-  return <div className="content">Sorry, but the manual is not available yet.</div>;
+  return <div className="page-content">Sorry, but the manual is not available yet.</div>;
 };
 export default Download;
